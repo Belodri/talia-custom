d40d32cb271c9dcd754b6fac9eb8bd1a22683d05
Dave <dave.belodri@gmail.com>
2024-06-09 17:40:20 +0200

    Added consumeItem helper function

--- scripts/_foundryHelpers.mjs
+++ scripts/_foundryHelpers.mjs
@@ -1,5 +1,7 @@
 export const _foundryHelpers = {
     getActorByUuid,
+    getActiveUserCharacters,
+    consumeItem,
     SECONDS: {
         IN_ONE_MINUTE: 60,
         IN_TEN_MINUTES: 600,
@@ -20,4 +22,57 @@ function getActorByUuid(uuid) {
     const actorToken = fromUuidSync(uuid);
     const actor = actorToken?.actor ? actorToken?.actor : actorToken;
     return actor;
+}
+
+/**
+ * Gets the assigned actors of all users currently online. 
+ * If the DM does not have an assigned actor, it will not include the DM's characters.
+ * @returns {Actor5e[]} Array of active user's characters
+ */
+function getActiveUserCharacters() {
+    //get active users
+    /** @type {User5e[]} */
+    const users = game.users.players.filter(user => user.active);
+    return users.map((user) => {
+        if(user.character) return user.character;
+    });
+}
+
+/**
+ * Consumes a specified amount of an item, updating its quantity or uses accordingly.
+ * If the item doesn't have uses, only consumes quantity.
+ * If the item has uses, it will decrement the uses first and decrease the quantity only when the uses are exhausted.
+ * The consumeCost parameter determines how much is consumed with each step.
+ * 
+ * @param {Item5e} item - The item to be consumed.
+ * @param {number} amountToConsume - The amount of the item to consume.
+ * @param {number} [consumeCost=1] - The cost per step for consuming the item.
+ * @returns {Promise|null} A promise that resolves when the item's quantity or uses have been updated, or null if not enough uses & quantity are available to be consumed.
+ */
+async function consumeItem(item, amountToConsume, consumeCost = 1) {
+    const actualConsume = amountToConsume * consumeCost;
+    const quantity = item.system.quantity;
+
+    if(!item.hasLimitedUses) {  //item does not have uses, only quantity
+        if(quantity < actualConsume) return null;
+        const quant = Math.max(quantity - actualConsume, 0);
+        return item.update({"system.quantity": quant});
+
+    } else {    //item has uses
+        const usesMax = item.system.uses.max;
+        let usesValue = item.system.uses.value;
+        let quant = quantity;
+
+        const maxPossibleUses = usesValue + usesMax * (quant - 1);  //how many uses are possible in total
+        if(maxPossibleUses < actualConsume) return null;
+
+        for(let i = 0; i < actualConsume; i++) {
+            usesValue--;
+            if(!usesValue) {
+                usesValue = usesMax;
+                quant--;
+            }
+        }
+        return item.update({"system.uses.value": usesValue, "system.quantity": Math.max(quant, 0)});
+    }
 }
