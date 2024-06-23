dcc9684be97385f1e7f3c815fc9469eeb4b6716b
Dave <dave.belodri@gmail.com>
2024-06-23 19:49:08 +0200

    shalkoc can cook on short rests & nobody can have more than 1 spice buff

--- shalkoc/cooking.mjs
+++ shalkoc/cooking.mjs
@@ -1,6 +1,4 @@
 import { _foundryHelpers } from "../scripts/_foundryHelpers.mjs";
-import { taliaEffectHelpers } from "../scripts/effects.mjs";
-import { talia_socket } from "../scripts/socket.mjs";
 
 const debug = false;
 
@@ -9,20 +7,22 @@ export function initCooking() {
 }
 export function setupCooking() {
     Hooks.on("dnd5e.restCompleted", async (actor, result) => {
-        await cookingMain(actor, result);
+        await cookingMain(actor);
         if(debug) console.log(actor, result);
     });
 }
 
 const spicesBuffsDatabase = {
-    //mode: 2 = add
-    //value: needs to be a string (I think)
+    /*
+    mode: 2 = add
+    value: needs to be a string (I think)
     "": {
         description: "<p></p>",
         changes: [
             {key: "", mode: 2, value: "", priority: 20},
         ]
     },
+    */
 
     "Star Anise": {
         description: "<p>You gain a +2 bonus to AC.</p>",
@@ -72,61 +72,69 @@ const spicesBuffsDatabase = {
 
 
 
-async function cookingMain(actor, result) {
-    //if(!result.longRest) return;    //only allow on long rests
-    if(!actor.name.includes("Shalkoc")) return;     //only allow Shalkoc to do that
+async function cookingMain(actor, forced = true) {
+    if(forced && !actor.name.includes("Shalkoc")) return;     //only allow Shalkoc to do that
 
-    //get chef's tools (to check if he has them and to use as the origin of the active effect later)
-    /** @type {Item5e | undefined} */
-    const chefTool = actor.items.find(i => i.system?.type?.baseItem === 'cook');
-    if(!chefTool) return;   //return early if no chef's tools are found
-//NOTIFICATION for missing chef's tools
+    //get chef feat (to use as effect origin later)
+    const chefFeat = actor.items.find(i => i.name === "Chef" && i.type === "feat");
+    if(!chefFeat && forced) return;
+    const originUuid = chefFeat.uuid || actor.uuid;
+    // TODO NOTIFICATION for missing chef feat
+
+    //get spices the user has on him with quantity > 0
+    //also check if the spices are listed in the spicesBuffsDatabase
 
     /** @type {Item5e[]} */
-    const spices = actor.items.filter(i => i.system?.type?.value === 'spices' && i.system?.quantity >= 1);
+    const spices = actor.items.filter(i => i.system?.type?.value === 'spices' 
+        && i.system?.quantity >= 1
+        && Object.keys(spicesBuffsDatabase).includes(i.name));
     if(spices.length === 0) return; //return early if he has no spices on him.
 
-
+    //let the user choose
     const chosenSpiceName = await userChosenSpice(spices);
 
     /** @type {Item5e} */
     const chosenSpice = spices.find(i => i.name === chosenSpiceName); 
 
-    //create the effect
-    const thisSpiceBuff = spicesBuffsDatabase[chosenSpice.name];
-    if(!thisSpiceBuff) {
-//NOTIFICATION
-        return;  //return early if no buff could be found
-    } 
-    /** @type {ActiveEffect} */
-    const effect = taliaEffectHelpers.createActiveEffect({
+    const effectData = {
         name: chosenSpice.name,
-        seconds: _foundryHelpers.SECONDS.IN_ONE_DAY,
-        description: thisSpiceBuff.description,
-        changes: thisSpiceBuff.changes,
-        icon: chosenSpice.img
-    });
+        duration: {
+            seconds: _foundryHelpers.SECONDS.IN_ONE_DAY,
+        },
+        description: spicesBuffsDatabase[chosenSpice.name].description,
+        changes: spicesBuffsDatabase[chosenSpice.name].changes,
+        icon: chosenSpice.img,
+        origin: originUuid,  //provide chef feat as origin so it's easy to filter out later
+    };
 
-    if(debug) console.log(effect);
-    //get active users
-    /** @type {User5e[]} */
-    const users = game.users.players.filter(user => user.active);
-    
-    try {
-        //let the GM apply the effect to every active player's character
-        for(let user of users) {
-            if(!user.character) continue;
-            const retProm = await talia_socket.executeAsGM('talia_addActiveEffect', {
-                effect: effect.toObject(),
-                uuid: user.character.uuid,
-                origin: chefTool.uuid,
-            });
+    await Requestor.request({
+        img: chosenSpice.img,
+        title: `Spicy Treat`,
+        description: `Shalkoc made a small treat filled with ${chosenSpice.name}.`,
+        buttonData: [{
+            label: `Eat`,
+            command: async function() {
+                //delete prior spice effect first
+                const prevEffect = actor.appliedEffects.find( e => spiceKeys.includes(e.name));
+                if(prevEffect) {
+                    await prevEffect.delete();
+                }
+                await ActiveEffect.create(effectData, {parent: actor});
+            },
+            scope: {
+                effectData: effectData,
+                spiceKeys: Object.keys(spicesBuffsDatabase),
+            }
+        }],
+        limit: Requestor.LIMIT.ONCE,
+        speaker: ChatMessage.implementation.getSpeaker({actor: actor}),
+        messageOptions: {
+            whisper: _foundryHelpers.getUserIdsArray(),
+            blind: true,
         }
-        //remove 1 quantity of that spice
-        await chosenSpice.update({"system.quantity": chosenSpice.system.quantity - 1});
-    } catch (error) {
-        ui.notifications.error("Error in Talia-Custom: cookingMain(). Please notify GM.");
-    }
+    });
+    await _foundryHelpers.consumeItem(chosenSpice, 1, 1, true);
+    return true;
 }
 
 
@@ -156,3 +164,52 @@ async function userChosenSpice(spices) {
     });
     return choice.chosen;
 }
+
+/*
+        const effect = taliaEffectHelpers.createActiveEffect({
+        name: chosenSpice.name,
+        seconds: _foundryHelpers.SECONDS.IN_ONE_DAY,
+        description: thisSpiceBuff.description,
+        changes: thisSpiceBuff.changes,
+        icon: chosenSpice.img
+    });
+
+    if(debug) console.log(effect);
+
+    //get active users
+    const users = game.users.players.filter(user => user.active);
+    
+
+     try {
+        //let the GM apply the effect to every active player's character
+        for(let user of users) {
+            if(!user.character) continue;
+            const character =  user.character;
+
+            //let the gm remove previous spice buff so only one can be active at any time
+            const prevEffect = character.appliedEffects.find( e => e.origin === chefFeatUuid);
+            if(prevEffect) {
+                await talia_socket.executeAsGM('talia_removeActiveEffect', )
+            }
+            
+            await talia_socket.executeAsGM('talia_addActiveEffect', {
+                effect: effect.toObject(),
+                uuid: character.uuid,
+                origin: chefFeatUuid,  //provide chef feat as origin so it's easy to filter out later
+            });
+
+            
+            
+
+        }
+        
+
+
+
+
+
+        //remove 1 quantity of that spice
+        await chosenSpice.update({"system.quantity": chosenSpice.system.quantity - 1});
+    } catch (error) {
+        ui.notifications.error("Error in Talia-Custom: cookingMain(). Please notify GM.");
+    } */
