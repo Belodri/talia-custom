dcc8a6ef848d6ed3083a7127cbb9e200f6c59f64
Dave <dave.belodri@gmail.com>
2024-08-25 16:03:51 +0200

    aspect of the wyrm fixes

--- features/character/shalkoc/aspectOfTheWyrm.mjs
+++ features/character/shalkoc/aspectOfTheWyrm.mjs
@@ -4,21 +4,21 @@ export default {
     register() {
         ItemHookManager.register("Aspect of the Wyrm", async (item) => {
             const config = {
-                consumeResource: item.system.uses.value === 0,
-                consumeUsage: item.system.uses.value !== 0,
+                consumeResource: item.system.uses.value === 0 ? true : null,
+                consumeUsage: item.system.uses.value !== 0 ? true : null,
             };
             const options = {
                 skipItemMacro: true,
-                configureDialog: false,
-                flags: {dnd5e: {use: {}}}
             };
+            
             if(config.consumeResource) {
-                options.flags.dnd5e.use.consumedUsage = true;
+                options["flags.dnd5e.use.consumedUsage"] = true;
             } else {
-                options.flags.dnd5e.use.consumedResource = true;
+                options["flags.dnd5e.use.consumedResource"] = true;
             }
+            
             await item.use(config, options);
-            ui.notifications.info(`Consumed ${consumeResource ? `${item.system.consume.amount} Ki` : "1 Use"}`);
+            ui.notifications.info(`Consumed ${config.consumeResource ? `${item.system.consume.amount} Ki` : "1 Use"}`);
         });
     }
 }
