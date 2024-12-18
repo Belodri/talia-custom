268f169cb7d704cd94ed8643128e721b409f0d09
Dave <33916281+Belodri@users.noreply.github.com>
2024-12-18 19:10:26 +0100

    Refactor: Grapple (#225)

--- features/shared/commonActions/grapple.mjs
+++ features/shared/commonActions/grapple.mjs
@@ -1,125 +1,77 @@
-/*  REQUIRES MODULE: 
-    - Rideable
-    - Requestor
-    - dFred's Convenient Effects
-*/
-
-import { TaliaCustomAPI } from "../../../scripts/api.mjs";
 import { MODULE } from "../../../scripts/constants.mjs";
+import ChatCardButtons from "../../../utils/chatCardButtons.mjs";
+import { Helpers } from "../../../utils/helpers.mjs";
 
 export default {
     register() {
-        TaliaCustomAPI.add({grapple: grappleItemMacro}, "ItemMacros");
-        Hooks.once("setup", () => {
-            const fields = [];
-            fields.push("flags.talia-custom.ignoreGrappleSizeLimit");
-            DAE.addAutoFields(fields);
+        ChatCardButtons.register({
+            itemName: "Grapple",
+            buttons: [
+                {
+                    label: "Roll Grapple",
+                    callback: async ({actor, message}) => {
+                        await _rollGrapple(actor, message);
+                    }
+                },
+                {
+                    label: "Contest Roll",
+                    callback: async ({message}) => {
+                        await _contestRoll(message);
+                    }
+                }
+            ]
         });
     }
 }
 
 /**
- * 
- * @param {Item} item 
- * @param {Token} actorToken 
+ * Rolls the higher of acr/ath against the DC in the message flag.
+ * If the message flag includes an actorUuid, that actor is used for the roll.
+ * @param {ChatMessage} message     The button's chat message.
  * @returns {void}
  */
-async function grappleItemMacro(item, actorToken) {
-    if(actorToken.actor.effects.find(e => e.name === "Grappling")) {
-        ui.notifications.warn("You can only grapple one target at a time.");
-        return null;
+async function _contestRoll(message) {
+    const flag = message.getFlag(MODULE.ID, "grappleFlag");
+    if(!flag) {
+        ui.notifications.warn("Please wait until the grappler has made their roll.");
+        return;
     }
 
-    //make sure exactly 1 token is targeted
-    if(game.user.targets.size !== 1) {
-        ui.notifications.warn("You need to select exactly one target.");
-        return null;
+    const actor = flag.targetActorUuid ? fromUuidSync(flag.targetActorUuid) : canvas.tokens?.controlled?.[0]?.actor;
+    if(!actor) return;
+
+    const actorRD = actor.getRollData();
+    const higherSkill = actorRD.skills.acr.total > actorRD.skills.ath.total ? "acr" : "ath";
+
+    const options = {
+        chooseModifier: false,
+    };
+    if( flag.isCritical ) options.fumble = 19;
+    else if( flag.isFumble ) options.critical = 2;
+    else options.targetValue = flag.grappleDC;
+
+    const roll = await actor.rollSkill(higherSkill, options);
+
+    if(Helpers.isRollSuccess(roll) === false) {
+        await game.dice3d.waitFor3DAnimationByMessageID(game.messages.contents.at(-1).id);
+        await actor.toggleStatusEffect("grappled", {active: true});
     }
-    const targetToken = game.user.targets.first();
+}
 
-    //display the item card before proceeding
-    await item.displayCard();
-
-    const targetRD = targetToken.actor.getRollData();
-    const actorRD = actorToken.actor.getRollData();
-
-    //check immunity
-    if(targetRD.traits.ci.value.has("grappled")) {
-        ui.notifications.warn("This creature is immune to being grappled.");
-        return null;
+/**
+ * Rolls the athletics check and stores the result, along with the target (optionally), in the message flag.
+ * @param {Actor} actor             The button's actor.
+ * @param {ChatMessage} message     The button's chat message.
+ * @returns {void}
+ */
+async function _rollGrapple(actor, message) {
+    const athRoll = await actor.rollSkill("ath");
+    if(typeof athRoll?.total !== "number") return;
+    const flag = {
+        grappleDC: athRoll.total,
+        isFumble: athRoll.isFumble,
+        isCritical: athRoll.isCritical,
+        targetActorUuid: game.user.targets.first()?.actor?.uuid ?? null,
     }
-
-    //check size diff
-    const grappleSizeIndex = (rd) => {
-        const baseSizeIndex = ['tiny', 'sm', 'med', 'lg', 'huge', 'grg'].indexOf(rd.traits.size);
-        const grappleSizeBonus = rd.flags?.["talia-custom"]?.grappleSizeBonus ? rd.flags?.["talia-custom"]?.grappleSizeBonus : 0;   //factor in flag "talia-custom.grappleSizeBonus"
-        const powerfulBuildBonus = rd.flags?.dnd5e?.powerfulBuild ? 1 : 0;  //factor in flag "dnd5e.powerfulBuild", add +1 to bonus if it's enabled
-        return baseSizeIndex + grappleSizeBonus + powerfulBuildBonus;
-    }
-    const actorGrappleSizeIndex = grappleSizeIndex(actorRD);
-    const targetGrappleSizeIndex = grappleSizeIndex(targetRD);
-    if((targetGrappleSizeIndex > actorGrappleSizeIndex + 1) && actorRD.getFlag(MODULE.ID, "ignoreGrappleSizeLimit") !== 1) {
-        ui.notifications.warn("This creature is too big for you to grapple.");
-        return null;
-    }
-
-    //get message data stuff
-    const speakerObj = ChatMessage.implementation.getSpeaker({actor: actorToken.actor});
-    const whisperArr = (() => {
-        const tSet = new Set();
-        game.users.forEach(user => {
-            if(targetToken.document.testUserPermission(user, "OWNER")) tSet.add(user.id)
-        });
-        return Array.from(tSet);
-    })();
-
-    //roll ath check
-    const athRoll = await actorToken.actor.rollSkill("ath");
-    await game.dice3d.waitFor3DAnimationByMessageID(game.messages.contents.at(-1).id);
-
-    //request apply grapple
-    await Requestor.request({
-        title: " ",
-        img: false,
-        buttonData: [{
-            label: "Apply Grapple",
-            scope: {
-                actorTokenId: actorToken.id,
-                targetTokenId: targetToken.id,
-                actorGrSize: actorGrappleSizeIndex,
-                targetGrSize: targetGrappleSizeIndex 
-            },
-            command: async function(){
-                const uuid = canvas.tokens.get(actorTokenId).actor.uuid;
-                const effectData = game.dfreds.effectInterface.findEffect({effectName: "Grappling"}).toObject();
-
-                //slow happens if the target is no more than 1 size smaller than the actor
-                if(targetGrSize >= actorGrSize - 1) {   
-                    effectData.changes = [{ key: "system.attributes.movement.all", value: "* 0.5", mode: 0, priority: 90 }];
-                }
-                
-                await game.dfreds.effectInterface.addEffect({effectData, uuid});
-                   
-                //effect macro on grappling condition then checks the token doc flags for which tokens are grappled and removes the grappled condition from them
-                game.Rideable.MountbyID([targetTokenId], actorTokenId, {Grappled: true})
-            }
-        }],
-        speaker: speakerObj,
-        whisper: whisperArr,
-    })
-
-    //request roll
-    const higherSkill = targetRD.skills.acr.total > targetRD.skills.ath.total ? "acr" : "ath";
-    await TaliaCustom.Helpers.requestRoll({
-        type: "skill",
-        ability: higherSkill === "acr" ? "dex" : "str",
-        skill: higherSkill, 
-        dc: athRoll.total, 
-        hideDC: false, 
-        messageOptions: {
-            speaker: speakerObj,
-            whisper: whisperArr,
-            flavor: "Roll Request: Grapple Contest"
-        }
-    });
+    await message.setFlag(MODULE.ID, "grappleFlag", flag);
 }
