6e5e312ca15291fa9c26d89c57c0ec4e5ad95f8e
Dave <dave.belodri@gmail.com>
2025-01-30 12:02:50 +0100

    Incorrect Step of the Wind duration Fixes #236

--- features/character/shalkoc/_shalkoc.mjs
+++ features/character/shalkoc/_shalkoc.mjs
@@ -1,11 +1,13 @@
 import aspectOfTheWyrm from "./aspectOfTheWyrm.mjs";
 import breathOfTheDragon from "./breathOfTheDragon.mjs";
 import martialStyleStances from "./martialStyleStances.mjs"
+import stepOfTheWind from "./stepOfTheWind.mjs";
 
 export default {
     registerSubsection() {
         martialStyleStances.register();
         breathOfTheDragon.register();
         aspectOfTheWyrm.register();
+        stepOfTheWind.register();
     }
 }
