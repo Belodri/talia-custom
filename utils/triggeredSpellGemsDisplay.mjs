0eea5a3121563eca7d7f5d3b476aff48a59ffe84
Dave <33916281+Belodri@users.noreply.github.com>
2025-03-25 12:16:37 +0100

    Rewrite TriggeredSpellGemDisplay (#302)

--- utils/triggeredSpellGemsDisplay.mjs
+++ utils/triggeredSpellGemsDisplay.mjs
@@ -1,76 +1,44 @@
-import Collection from "../foundry/common/utils/collection.mjs";
 import { TaliaCustomAPI } from "../scripts/api.mjs";
 import { MODULE } from "../scripts/constants.mjs";
 
 export default {
     register() {
         GemDisplay.init();
+        TaliaCustomAPI.add({GemDisplay}, "Other");
     }
 }
 
 class GemDisplay {
-    static displayOptions = {
+    static CONFIG = {
+        triggerConditionFlag: "spellGem.triggerCondition",
+        itemTypeValue: "spellGem",
+        debounceDelay: 50,
         anchorId: "hotbar-page-controls",
+        defaultContent: "<p>None</p>",
         styles: {
             "min-width": "fit-content",
             "padding-inline": '10px',
-            backgroundColor: 'rgba(0, 0, 0, 0.7)',
-            color: 'white',
-            borderRadius: '10px',
+            "backgroundColor": 'rgba(0, 0, 0, 0.7)',
+            "color": 'white',
+            "borderRadius": '10px',
             "align-self": "end",
+            "display": "block",
+            "text-wrap": "nowrap",
         },
+        maxLines: 6,
     }
 
-    static #active = true;
+    /** @type {GemDisplay} */
+    static #instance = null;
 
-    static get active() { return GemDisplay.#active; }
-
-    static set active(value) {
-        if(typeof value !== "boolean") throw new TypeError(`Value must be boolean`);
-
-        GemDisplay.#active = value;
-
-        if(GemDisplay.element) {
-            GemDisplay.refresh();
-
-            GemDisplay.element.style.display = value 
-                ? "block"
-                : "none";
-        }
-    }
-
-    /** @type {HTMLDivElement} */
-    static element = null;
-
-    /** @type {Set<string>} */
-    static pcUuids = new Set();
-
-    /** 
-     * A map with actorUuids of player characters as keys and the corresponding display string as values
-     * @type {Collection<string, string>} 
-     */
-    static actorTexts = new foundry.utils.Collection();
+    static get active() { return !!GemDisplay.#instance; }
 
     static init() {
         Hooks.on("getSceneControlButtons", GemDisplay._onGetSceneControlButtonsHook);
-        Hooks.once("ready", GemDisplay._onReadyHook);
     }
 
-    static _onReadyHook() {
-        GemDisplay.pcUuids = new Set( 
-            game.users.players.map(u => u.character.uuid) 
-        );
-
-        game.users.players
-            .map(u => u.character)
-            .filter(a => game.user.isGM || a.uuid === game.user.character?.uuid)
-            .forEach(a => GemDisplay.setActorText(a));
-
-        GemDisplay.updateDisplayText();
-        
-        Hooks.on("updateItem", (item, data, options, userId) => GemDisplay._onItemChange(item, userId));
-        Hooks.on("createItem", (item, options, userId) => GemDisplay._onItemChange(item, userId));
-        Hooks.on("deleteItem", (item, options, userId) => GemDisplay._onItemChange(item, userId));
+    static configure(newConfig) {
+        this.CONFIG = foundry.utils.mergeObject(this.CONFIG, newConfig);
     }
 
     /** @param {SceneControl} controls*/
@@ -82,111 +50,203 @@ class GemDisplay {
             icon: "fas fa-gem",
             toggle: true,
             active: GemDisplay.active,
-            onClick: toggled => GemDisplay.active = toggled,
+            onClick: toggled => GemDisplay.toggle(toggled),
             button: true
         });
     }
 
+    /** @param {boolean} value */
+    static toggle(value) {
+        return value ? GemDisplay.#create() : GemDisplay.#destroy();
+    }
 
-    static initDisplayElement() {
-        const opts = GemDisplay.displayOptions;
+    static #create() {
+        GemDisplay.#destroy();
 
-        // Find the anchor element
-        const anchorElement = document.getElementById(opts.anchorId);
+        const instance = new GemDisplay();
+        instance._onCreateInstance();
+        GemDisplay.#instance = instance;
+    }
+
+    static #destroy() {
+        if(GemDisplay.#instance) {
+            GemDisplay.#instance?._onDestroyInstance();
+            GemDisplay.#instance = null;
+        }
+    }
+
+    /*----------------------------------------------------------------------------
+                    Instance          
+    ----------------------------------------------------------------------------*/
+
+    /** @type {{[key: string]: number}} */
+    #hookIds = {
+        createItem: null,
+        updateItem: null,
+        deleteItem: null,
+        canvasReady: null,
+    };
+
+    /** @type {Map<string, Item>} */
+    #items = new Map();
+
+    /** @type {HTMLDivElement} */
+    #element = null;
+
+    constructor() {
+        /** @type {boolean} */
+        this.isGM = game.user.isGM;
+        /** @type {string | null} */
+        this.userCharUuid = game.user.character?.uuid ?? null;
+
+        // Proxy to trigger a refresh whenever the map is modified.
+        this.#items = new Proxy(this.#items, {
+            get: (target, prop, receiver) => {
+                if (['set', 'delete', 'clear'].includes(prop)) {
+                    return function(...args) {
+                        const retVal = target[prop](...args);
+                        this.setElementContent();
+                        return retVal;
+                    }.bind(this);
+                }
+
+                const value = Reflect.get(target, prop, receiver);
+                return typeof value === 'function' 
+                    ? value.bind(target) 
+                    : value;
+            }
+        });
+    }
+
+    _onCreateInstance() {
+        // register hooks
+        this.#hookIds.createItem = Hooks.on("createItem", this._onCreateItem.bind(this));
+        this.#hookIds.updateItem = Hooks.on("updateItem", this._onUpdateItem.bind(this));
+        this.#hookIds.deleteItem = Hooks.on("deleteItem", this._onDeleteItem.bind(this));
+        this.#hookIds.canvasReady = Hooks.on("canvasReady", this._onCanvasReady.bind(this));
+
+        //create display
+        this.#createDisplayElement();
+
+        // populate items
+        this.#populateItems();
+    }
+
+    _onDestroyInstance() {
+        // deregister hooks
+        for(const [hookName, id] of Object.entries(this.#hookIds)) {
+            if(id) Hooks.off(hookName, id);
+        }
+        if(this.#element) this.#element.remove();
+    }
+
+    /*----------------------------------------------------------------------------
+                    Events            
+    ----------------------------------------------------------------------------*/
+
+    _onCreateItem(item, options, userId) {
+        if(!this.#validateItem(item, {checkUserChar: true, checkEmbedded: true, equippedEquals: true})) return;
+        this.#items.set(item.uuid, item);
+    }
+
+    _onUpdateItem(item, changed, options, userId) {
+        if(!this.#validateItem(item, {checkUserChar: true, checkEmbedded: true})) return;
+
+        const equipped = changed.system?.equipped;
+        if(equipped === true) this.#items.set(item.uuid, item);
+        else if(equipped === false) this.#items.delete(item.uuid);
+    }
+
+    _onDeleteItem(item, options, userId) {
+        if(!this.#validateItem(item, {checkUserChar: true})) return;
+        this.#items.delete(item.uuid);
+    }
+
+    _onCanvasReady(canvas) {
+        // Defer execution to next macrotask so other _onCanvasReady hooks aren't blocked.
+        setTimeout(() => this.#populateItems(), 0); 
+    }
+
+    /*----------------------------------------------------------------------------
+                    Item Handling            
+    ----------------------------------------------------------------------------*/
+
+    #populateItems() {
+        this.#items.clear();
+
+        for(const t of canvas?.scene?.tokens ?? []) {
+            const validToken = t?.actor && (this.isGM || t.actor?.uuid === this.userCharUuid);
+            if(!validToken) continue;
+
+            for(const i of t.actor.items) {
+                if( this.#validateItem(i, {equippedEquals: true}) ) {
+                    this.#items.set(i.uuid, i);
+                }
+            }
+        }
+    }
+
+    #validateItem(item, {checkUserChar=false, checkEmbedded=false, equippedEquals=null}={}) {
+        // Is the item a valid triggered spell gem?
+        const isSpellGem = item?.system?.type?.value === GemDisplay.CONFIG.itemTypeValue;
+        const hasTrigger = !!this.#getCondition(item);
+        if(!isSpellGem || !hasTrigger) return false;
+
+        // Is the item embedded on an actor?
+        if(checkEmbedded && !item.actor) return false;
+
+        // What's the equipment status of the item?
+        if(equippedEquals !== null && item.system.equipped !== equippedEquals) return false;
+
+        // If the user is not a GM, is the item on the user's assigned character?
+        if(checkUserChar && !this.isGM && item?.actor?.uuid !== this.userCharUuid) return false;
+
+        return true;
+    }
+
+    #getCondition(item) {
+        return item?.getFlag(MODULE.ID, GemDisplay.CONFIG.triggerConditionFlag);
+    }
+
+    /*----------------------------------------------------------------------------
+                    Display            
+    ----------------------------------------------------------------------------*/
+
+    #createDisplayElement() {
+        const anchorElement = document.getElementById(GemDisplay.CONFIG.anchorId);
         if (!anchorElement) {
-            console.error(`Element with id "${opts.anchorId}" not found`);
-            return;
+            throw new Error(`Anchor element with id "${GemDisplay.CONFIG.anchorId}" not found`);
         }
 
-        // Create the element div
-        GemDisplay.element = document.createElement('div');
-        
-        // Apply all styles
-        Object.assign(GemDisplay.element.style, opts.styles);
+        this.#element = document.createElement('div');
+        this.#element.innerHTML = GemDisplay.CONFIG.defaultContent;
+        Object.assign(this.#element.style, GemDisplay.CONFIG.styles);
 
-        // Insert after the anchor element
         if (anchorElement.nextSibling) {
-            anchorElement.parentNode.insertBefore(GemDisplay.element, anchorElement.nextSibling);
+            anchorElement.parentNode.insertBefore(this.#element, anchorElement.nextSibling);
         } else {
-            anchorElement.parentNode.appendChild(GemDisplay.element);
+            anchorElement.parentNode.appendChild(this.#element);
         }
     }
 
-    static refresh() {
-        game.users.players
-            .map(u => u.character)
-            .filter(a => game.user.isGM || a.uuid === game.user.character?.uuid)
-            .forEach(a => GemDisplay.setActorText(a));
-        GemDisplay.updateDisplayText();
+    #getParagraph(item) {
+        const itemName =  item.name.startsWith("Triggered: ") && item.name.indexOf(" - ") > 0
+            ? item.name.split(" - ")[0].replace("Triggered: ", "")
+            : item.name;
+
+        const condition = this.#getCondition(item) ?? "NO TRIGGER CONDITION"; // Should never happen but just in case
+        const charName = item.actor.token?.name ?? item.actor.name.split(" ")[0];   // use token name for synthetic actors to keep them apart
+
+        return `<p><b>${charName}:</b> ${itemName} - <i>"${condition}"</i></p>`;
     }
 
-    static updateDisplayText() {
-        if(!GemDisplay.element) {
-            GemDisplay.initDisplayElement();
-        }
+    setElementContent = foundry.utils.debounce(() => {
+        const content = [...this.#items.values()]
+            .map(i => this.#getParagraph(i))
+            .sort((a, b) => a.localeCompare(b))
+            .slice(0, GemDisplay.CONFIG.maxLines)
+            .join("");
 
-        const displayText = GemDisplay.actorTexts
-            .filter(Boolean)
-            .map(v => v)
-            .join("</br>");
-        
-        GemDisplay.element.innerHTML = `<p>${displayText}</p>` || "<p>None</p>"
-    }
-
-    /** 
-     * @param {Item} item 
-     * @param {object} data
-     * @param {object} options
-     * @param {string} userId   
-     */
-    static _onItemChange(item, data, options, userId) {
-        //only gm should track others
-        if(userId !== game.userId && !game.user.isGM) return;   
-
-        if( GemDisplay.#isItemSpellGem(item) && GemDisplay.#isItemOnPC(item) ) {
-            GemDisplay.setActorText(item.actor);
-            GemDisplay.updateDisplayText();
-        }
-    }
-
-    /** @param {Item} item */
-    static #isItemOnPC(item) { return GemDisplay.pcUuids.has(item?.actor?.uuid); }
-
-    /** @param {Item} item */
-    static #isItemSpellGem(item) { return item?.system.type?.value === "spellGem"; }
-
-    /** 
-     * @param {Actor} actor 
-     * @returns {Item[]}
-     */
-    static getEquippedTriggeredSpellGems(actor) {
-        return actor.items.filter(i => 
-            i.type === "consumable" 
-            && i.system.type?.value === "spellGem" 
-            && i.system.equipped 
-            && i.name.startsWith("Triggered: ")
-        );
-    }
-
-    /** @param {Actor} actor */
-    static setActorText(actor) {
-        const gems = GemDisplay.getEquippedTriggeredSpellGems(actor);
-        const firstName = actor.name.split(" ")[0];
-
-        let displayText = "";
-        if(gems.length === 1) {
-            const triggerCond = gems[0].getFlag("talia-custom", "spellGem.triggerCondition") ?? "NO TRIGGER CONDITION";
-            const trimmedName = gems[0].name.replace("Triggered: ", "");
-
-            displayText = game.user.isGM 
-                ? `${firstName} (1): ${trimmedName} ["${triggerCond}"]`
-                : `${trimmedName} ["${triggerCond}"]`;
-        } else if(gems.length > 1) {
-            displayText = game.user.isGM 
-                ? `${firstName} (${gems.length}): INVALID (more than 1 triggered spell gem equipped)`
-                : `INVALID (You cannot have more than 1 triggered spell gem equipped!)`;
-        }
-
-        GemDisplay.actorTexts.set(actor.uuid, displayText);
-    }
+        this.#element.innerHTML = content || GemDisplay.CONFIG.defaultContent;
+    }, GemDisplay.CONFIG.debounceDelay) 
 }
