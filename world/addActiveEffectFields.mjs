b324033bb968a1924456e36933e2552a3af4d9c8
Dave <dave.belodri@gmail.com>
2025-04-04 23:26:45 +0200

    Enhance spell level modification by adding filtering options and refactoring field registration

--- world/addActiveEffectFields.mjs
+++ world/addActiveEffectFields.mjs
@@ -1,30 +1,27 @@
 export default {
     register() {
         Hooks.once("setup", () => {
-            const fields = [];
-            getDaeAutoFields(fields);
-            DAE.addAutoFields(fields);
+            addFields();
         });
     }
 }
 
-/**
- * 
- * @param {[]} fields   Gets mutated;
- */
-function getDaeAutoFields(fields) {
-    // add modifySpellLevel.spellSchools._ fields
-    fields.push(
-        ...Object.keys(CONFIG.DND5E.spellSchools)
-            .map(k => `flags.talia-custom.modifySpellLevel.spellSchools.${k}`)
-    );
+/** Adds active effect fields to DAE */
+function addFields() {
+    DAE.addAutoFields([
+        // add modifySpellLevel fields
+        "flags.talia-custom.modifySpellLevel.all",
+        "flags.talia-custom.modifySpellLevel.filter.underLevel",
+        "flags.talia-custom.modifySpellLevel.filter.overLevel",
 
-    //add jump dist fields
-    const jumpDistFields = [
+        // add modifySpellLevel.spellSchools._ fields
+        ...Object.keys(CONFIG.DND5E.spellSchools)
+            .map(k => `flags.talia-custom.modifySpellLevel.spellSchools.${k}`),
+
+        //add jump dist fields
         "flags.talia-custom.jumpDist.bonus",
         "flags.talia-custom.jumpDist.countDoubled",
         "flags.talia-custom.jumpDist.countHalved",
-        "flags.talia-custom.jumpDist.distMult"
-    ];
-    fields.push(...jumpDistFields);
+        "flags.talia-custom.jumpDist.distMult",
+    ]);
 }
