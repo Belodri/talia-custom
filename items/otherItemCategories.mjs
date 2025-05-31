3b062759751583a79e45d9c5d9bb89aa17a22a42
Dave <dave.belodri@gmail.com>
2025-05-31 23:07:25 +0200

    Change exotic currency type to consumable

--- items/otherItemCategories.mjs
+++ items/otherItemCategories.mjs
@@ -1,15 +1,15 @@
 export default {
     register() {
-        registerLootCategories();
+        registerCategories();
     }
 }
 
 /**
  *
  */
-function registerLootCategories() {
+function registerCategories() {
     CONFIG.DND5E.lootTypes.livestock = {label: "Livestock"};
     CONFIG.DND5E.lootTypes.wood = {label: "Logs and Lumber"};
     CONFIG.DND5E.lootTypes.stone = {label: "Stone"};
-    CONFIG.DND5E.lootTypes.exoticCurrency = {label: "Exotic Currency"};
+    CONFIG.DND5E.consumableTypes.exoticCurrency = {label: "Exotic Currency"}
 }
