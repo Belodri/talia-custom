03661d073992b0121f55da757e22d60d4452ccc7
Dave <dave.belodri@gmail.com>
2025-06-01 12:37:32 +0200

    Remove Potion of Immediate Rest from alchemy recipes

--- features/professions/alchemy/alchemy.mjs
+++ features/professions/alchemy/alchemy.mjs
@@ -210,10 +210,6 @@ const RECIPES = {
         {name: "Snow Moss", quantity: 2}, 
         {name: "Essence of Legend", quantity: 1}
     ]},
-    potionOfImmediateRest: {name: "Potion of Immediate Rest", rarity: "legendary", ingredients: [
-        {name: "Dragon Scale", quantity: 2}, 
-        {name: "Volcano Poppy", quantity: 1}
-    ]},
     potionOfLegendaryResistance: {name: "Potion of Legendary Resistance", rarity: "legendary", ingredients: [
         {name: "Essence of Legend", quantity: 3}
     ]},
