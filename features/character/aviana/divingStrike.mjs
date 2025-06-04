187cbb3a3ce9bef09674339817920d3846720e11
Dave <dave.belodri@gmail.com>
2025-06-04 21:23:41 +0200

    Refactor Diving Strike class to improve crosshair functionality

--- features/character/aviana/divingStrike.mjs
+++ features/character/aviana/divingStrike.mjs
@@ -1,201 +1,227 @@
-// functions are called via item macro chat buttons
-
-import { TaliaCustomAPI } from "../../../scripts/api.mjs";
-import { Jump } from "../../shared/commonActions/jump.mjs";
+import ChatCardButtons from "../../../utils/chatCardButtons.mjs";
+import Mover from "../../../utils/Mover.mjs";
 
 export default {
     register() {
-        TaliaCustomAPI.add({ChatButtons_DivingStrike}, "ItemMacros");
+        ChatCardButtons.register({
+            itemName: "Diving Strike",
+            isPartialName: true,
+            displayFilter: (item) => item.actor?.itemTypes?.subclass?.some(s => s.name === "Spirit Warrior"),
+            buttons: [{
+                label: "Jump",
+                callback: ({token, item}) => new DivingStrike(token, item).runMacro()
+            }]
+        });
     }
 }
 
-const ChatButtons_DivingStrike = {
-    jump
-}
-
-/**
- * Is rectA fully contained within, or equal to, rectB? 
- * @param {PIXI.Rectangle} rectA 
- * @param {PIXI.Rectangle} rectB 
- * @returns {boolean}
- */
-function _fits(rectA, rectB) {
-    return ( rectA.bottom <= rectB.bottom 
-        && rectA.left <= rectB.left
-        && rectA.right <= rectB.right
-        && rectA.top <= rectB.top
-    )
-}
-
-/**
- * Checks if the crosshair shape is fully contained, or equals, the placeable object
- * or the other way around.
- * @param {PlaceableObject} placeable       The token object
- * @param {CrosshairsDocument} crosshair 
- * @param {PIXI.Graphics} shape 
- * @returns {boolean}
- */
-function _filterMethod(placeable, crosshair, shape) {
-    const shapeBounds = shape.getBounds() ?? null;
-    return shapeBounds ? ( _fits(placeable.bounds, shapeBounds) || _fits(shapeBounds, placeable.bounds) ) : false
-}
-
-/**
- *
- */
-async function getAndVerifyLocation(token, maxJumpDistInFt, minSpacingInFt) {
-    const tWidthInGU = Math.max(1, token.document.width);   //token width in GU; minimum of 1;
-
-    const location = await Sequencer.Crosshair.show({
-        location: {
-            obj: token,
-            limitMaxRange: maxJumpDistInFt - 1,
-            limitMinRange: minSpacingInFt + 6,
-            showRange: true,
-            wallBehavior: Sequencer.Crosshair.PLACEMENT_RESTRICTIONS.ANYWHERE,
-            displayRangePoly: true,
-            rangePolyLineColor: 0o000000,
-            rangePolyLineAlpha: 1,
-        },
-        gridHighlight: true,
-        snap: {
-            position: tWidthInGU % 2 === 0 ? CONST.GRID_SNAPPING_MODES.VERTEX : CONST.GRID_SNAPPING_MODES.CENTER
-        }    
-    });
-    await Sequencer.Helpers.wait(500);  //wait a little so the control works properly
-    token.control();
-    if(location === false) return null; // false means it's been cancelled so we need to break the loop.
-
-    const col = Sequencer.Crosshair.collect(location, "Token", _filterMethod);
-    if(!col.length || col[0]._id === token._id) {
-        ui.notifications.info("You need to select a location which is occupied by a token.");
-        return await getAndVerifyLocation(token, maxJumpDistInFt, minSpacingInFt);
+class DivingStrike extends Mover {
+    static CONFIG = {
+        jump: { minSpacing: 10 },
+        bonus: { 
+            diceSize: "d6",
+            distanceInterval: 10
+        }
     }
-    return location;
-}
 
-/**
- *
- */
-async function updateBabonus(item, pointA, pointB) {
-    //calculate damage
-    const bonusDamageDiceSize = "d6";
-    const path = canvas.grid.measurePath([pointA, pointB], {});
-    const dice = Math.floor(path.distance/10);
-    const damageDiceString = dice ? `${dice}${bonusDamageDiceSize}` : "0";
-
-    // get bonus
-    const col = babonus.getCollection(item);
-    const bonus = col.find(b => b.name === item.name);
-
-    // update if needed
-    if(bonus.bonuses.bonus !== damageDiceString) {
-        await bonus.update({"bonuses.bonus": damageDiceString});
+    /**
+     * @param {Token} token 
+     * @param {Item} item 
+     */
+    constructor(token, item) {
+        super(token);
+        this.item = item;
+        this.startCenter = this.token.center;
     }
-    return ui.notifications.info(`Your ${path.distance}ft leap adds ${damageDiceString} damage to the attack.`);
-}
 
-/**
- *
- */
-async function jump(token, item) {
-    const maxJumpDistInFt = item.actor.getRollData().talia.jumpDistance;
-    const minSpacingInFt = 5;
+    /** @type {number} Number to keep track of the last sent notification. */
+    #notifId;
 
-    //get location 
-    const location = await getAndVerifyLocation(token, maxJumpDistInFt, minSpacingInFt);
+    /** @type {PIXI.Rectangle[]} */
+    #tokenBounds;
 
-    if(!location) return;   //if jump is cancelled
-    const targetPosition = {
-        x: location.x,
-        y: location.y
-    };
+    /**
+     * Gets the bounds of all visible tokens on the scene except the executing token.
+     * @returns {PIXI.Rectangle[]} 
+     */
+    get tokenBounds() {
+        if(!this.#tokenBounds) {
+            this.#tokenBounds = this.token.document.collection
+                .filter(tDoc => !tDoc.hidden && tDoc._id !== this.token.document._id)
+                .map(tDoc => tDoc.object.bounds);
+        }
+        return this.#tokenBounds;
+    }
 
-    await Promise.all([
-        updateBabonus(item, token.center, targetPosition),
-        playJumpAnimation(token, targetPosition)
-    ]);
+    /**
+     * Runs the Diving Strike macro sequence.
+     * @returns {Promise<void>}
+     */
+    async runMacro() {
+        const maxJumpDist = this.item.actor.getRollData().talia.jumpDistance;
+        const minJumpDist = (this.token.document.width * this.scene.grid.distance) 
+            + this.constructor.CONFIG.jump.minSpacing;
 
-    await Jump.setElevationToGround(token, targetPosition);
-}
+        this.setCrosshairOptions({
+            "location.limitMaxRange": maxJumpDist,
+            "location.limitMinRange": minJumpDist
+        });
 
-/**
- *
- */
-async function playJumpAnimation(token, position) {
-    await new Sequence()
-        .canvasPan()
-        .delay(100)
-        .shake({duration: 500, strength: 2, rotation: true, fadeOut:500})
+        this.setCrosshairCallbacks({
+            [Sequencer.Crosshair.CALLBACKS.MOVE]: this.#onCrosshairMove.bind(this),
+            [Sequencer.Crosshair.CALLBACKS.INVALID_PLACEMENT]: this.#onCrosshairInvalidPlacement.bind(this)
+        });
 
-        .sound()
-        .file("TaliaCampaignCustomAssets/c_sounds/DivingStrike.mp3")
-        .atLocation(token)
-        .radius(120)    //can hear within 120ft
-        .distanceEasing(true)
+        const loc = await this.getLocation();
+        if(!loc) return;
 
-        .effect()
-        .file("jb2a.impact.ground_crack.orange.01")
-        .startTime(300)
-        .scale(0.3)
-        .randomRotation()
-        .atLocation(token)
-        .belowTokens()
+        const res = await this.setTarget(loc)
+            .executeMode("JUMP");
+        if(!res) return;
 
-        .effect()
-        .file("jb2a.smoke.puff.side.02.white.1")
-        .atLocation(token)
-        .rotateTowards(position)
-        .rotate(180)
-        .belowTokens()
+        await this.#updateBab();
+    }
 
-        .animation()
-        .on(token)
-        .opacity(0)
+    /**
+     * Displays notification on invalid crosshair placement, 
+     * and returns user control over the token.
+     * Avoids duplicate messages.
+     * @returns {void}
+     */
+    #onCrosshairInvalidPlacement() {
+        const notifActive = ui.notifications.active.some(li => li.data("id") === this.#notifId);
+        if(!notifActive) this.#notifId = ui.notifications.info("You need to select a location which is occupied by a token.");
+        this.#debounceControl();
+    }
 
-        .effect()   //salto
-        .from(token)
-        .scaleOut(3, 1500, {ease: "easeOutQuint"})
-        .fadeOut(800, {ease: "easeOutQuint"})
-        .rotateTowards(position, { rotate: false })
-        .animateProperty("sprite", "position.x", { from: 0, to: 1, duration: 1250, gridUnits: true, ease: "easeOutQuint"})
-        .waitUntilFinished()
+    /**
+     * Returns control over the token upon invalid placement.
+     */
+    #debounceControl = foundry.utils.debounce(() => this.token.control(), 200);
 
-        .animation()
-        .on(token)
-        .teleportTo(position)
-        .snapToGrid()
+    /**
+     * Tests if the crosshair overlaps with any token during flight.
+     * @param {object} crosshair
+     * @returns {void}
+     */
+    #onCrosshairMove(crosshair) {
+        crosshair.isValid = this.tokenBounds
+            .some(tb => this.#interiorsIntersect(tb, crosshair.bounds));
+    }
 
-        .effect()   //queda
-        .from(token)
-        .atLocation(position)
-        .scaleIn(3, 1200, {ease: "easeInCubic"})
-        .fadeIn(600, {ease: "easeInCubic"})
-        .rotateTowards(token, { rotate: false })
-        .animateProperty("sprite", "position.x", { from: 3, to: -0.5, duration: 1250, gridUnits: true, ease: "easeInCubic"})
-        .waitUntilFinished(-50)
+    /**
+     * Test if the interior of rectangle t intersects with the interior of rectangle c.
+     * Sharing an edge is not considered an intersection for this purpose.
+     * @param {PIXI.Rectangle} t
+     * @param {PIXI.Rectangle} c
+     * @returns {boolean}
+     */
+    #interiorsIntersect(t, c) {
+        return t.left < c.right
+            && t.right > c.left
+            && t.top < c.bottom
+            && t.bottom > c.top;
+    }
 
-        .effect()
-        .file("jb2a.smoke.puff.ring.01.white.1")
-        .randomRotation()
-        .atLocation(position)
-        .belowTokens()
+    /**
+     * Updates the damage bonus on on the item based on the distance of the jump.
+     */
+    async #updateBab() {
+        //calculate damage
 
-        .effect()
-        .file("jb2a.impact.ground_crack.orange.01")
-        .startTime(300)
-        .randomRotation()
-        .atLocation(position)
-        .belowTokens()
+        const { diceSize, distanceInterval } = this.constructor.CONFIG;
+        const path = canvas.grid.measurePath([this.startCenter, this.token.center], {});
+        const diceCount = Math.floor(path.distance/distanceInterval);
+        const damageDiceString = diceCount ? `${diceCount}${diceSize}` : "0";
 
-        .canvasPan()
-        .delay(100)
-        .shake({duration: 1200, strength: 3, rotation: true, fadeOut:500})
-        
-        .animation()
-        .delay(50)
-        .on(token)
-        .opacity(1)
+        // get bonus
+        const col = babonus.getCollection(this.item);
+        const bonus = col.find(b => b.name === this.item.name);
+
+        // update if needed
+        if(bonus.bonuses.bonus !== damageDiceString) {
+            await bonus.update({"bonuses.bonus": damageDiceString});
+        }
+
+        ui.notifications.info(`Your ${path.distance}ft leap adds ${damageDiceString} damage to the attack.`);
+    }
+
+    /** @override */
+    async _jump() {
+        await new Sequence()
+            .canvasPan()
+            .delay(100)
+            .shake({duration: 500, strength: 2, rotation: true, fadeOut:500})
+
+            .sound()
+            .file("TaliaCampaignCustomAssets/c_sounds/DivingStrike.mp3")
+            .atLocation(this.token)
+            .radius(120)    //can hear within 120ft
+            .distanceEasing(true)
+
+            .effect()
+            .file("jb2a.impact.ground_crack.orange.01")
+            .startTime(300)
+            .scale(0.3)
+            .randomRotation()
+            .atLocation(this.token)
+            .belowTokens()
+
+            .effect()
+            .file("jb2a.smoke.puff.side.02.white.1")
+            .atLocation(this.token)
+            .rotateTowards(this.targetPos)
+            .rotate(180)
+            .belowTokens()
+
+            .animation()
+            .on(this.token)
+            .opacity(0)
+
+            .effect()   //salto
+            .from(this.token)
+            .scaleOut(3, 1500, {ease: "easeOutQuint"})
+            .fadeOut(800, {ease: "easeOutQuint"})
+            .rotateTowards(this.targetPos, { rotate: false })
+            .animateProperty("sprite", "position.x", { from: 0, to: 1, duration: 1250, gridUnits: true, ease: "easeOutQuint"})
+            .waitUntilFinished()
+
+            .animation()
+            .on(this.token)
+            .teleportTo(this.targetPos)
+            .snapToGrid()
+
+            .effect()   //queda
+            .from(this.token)
+            .atLocation(this.targetPos)
+            .scaleIn(3, 1200, {ease: "easeInCubic"})
+            .fadeIn(600, {ease: "easeInCubic"})
+            .rotateTowards(this.token, { rotate: false })
+            .animateProperty("sprite", "position.x", { from: 3, to: -0.5, duration: 1250, gridUnits: true, ease: "easeInCubic"})
+            .waitUntilFinished(-50)
+
+            .effect()
+            .file("jb2a.smoke.puff.ring.01.white.1")
+            .randomRotation()
+            .atLocation(this.targetPos)
+            .belowTokens()
+
+            .effect()
+            .file("jb2a.impact.ground_crack.orange.01")
+            .startTime(300)
+            .randomRotation()
+            .atLocation(this.targetPos)
+            .belowTokens()
+
+            .canvasPan()
+            .delay(100)
+            .shake({duration: 1200, strength: 3, rotation: true, fadeOut:500})
             
-        .play()
+            .animation()
+            .delay(50)
+            .on(this.token)
+            .opacity(1)
+                
+            .play()
+    }
 }
