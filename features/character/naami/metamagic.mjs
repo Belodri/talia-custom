374d9af27d72757f3f31d6b4f8bcf34e0b1f9dbd
Dave <dave.belodri@gmail.com>
2025-08-01 21:42:34 +0200

    refactor metamagic and implement extended spell

--- features/character/naami/metamagic.mjs
+++ features/character/naami/metamagic.mjs
@@ -1,38 +1,48 @@
 /* eslint-disable jsdoc/require-jsdoc */
+
 import ChatCardButtons from "../../../utils/chatCardButtons.mjs";
-import { ItemHookManager } from "../../../utils/ItemHookManager.mjs";
 import ChanneledMetamagic from "./channeledMetamagic.mjs";
 
 export default {
     register() {
-        heightened();
-        twinned();
-        subtle();
+        METAMAGICS.forEach(config => {
+            config.registerButtonFn?.(config.itemName);
+        });
     }
 }
 const rollFatigueButton = { label: "Roll Channeling Fatigue", callback: ChanneledMetamagic.channel };
 
+const METAMAGICS = [
+    { itemName: "Metamagic: Careful Spell", registerButtonFn: defaultButton },
+    { itemName: "Metamagic: Distant Spell", registerButtonFn: defaultButton },
+    { itemName: "Metamagic: Subtle Spell", registerButtonFn: defaultButton },
+    { itemName: "Metamagic: Heightened Spell", registerButtonFn: heightened },
+    { itemName: "Metamagic: Twinned Spell", registerButtonFn: twinned },
+    { itemName: "Metamagic: Extended Spell", registerButtonFn: extended },
+]
 
-function subtle(itemName = "Metamagic: Subtle Spell") {
+
+function defaultButton(itemName) {
     ChatCardButtons.register({ itemName, buttons: [
         rollFatigueButton
     ]});
 }
 
+
 function heightened(itemName = "Metamagic: Heightened Spell") {
     ChatCardButtons.register({ itemName, buttons: [
         { label: "Warded", callback: ({item}) => heightenedButton(item, false) },
-        { label: "Channeled", callback: ({item}) => heightenedButton(item, true) },
+        { label: "Channelled", callback: ({item}) => heightenedButton(item, true) },
         rollFatigueButton
     ]});
 
     /**
      * @param {Item5e} item 
-     * @param {boolean} channeled 
+     * @param {boolean} channelled 
      */
-    async function heightenedButton(item, channeled=false) {
+    async function heightenedButton(item, channelled=false) {
         const eff = item.effects
-            .find(e => e.name.includes(channeled ? "(Channeled)" : "(Warded)"));
+            .find(e => e.name.includes(channelled ? "(Channelled)" : "(Warded)"));
         await eff.update({disabled: false});
 
         Hooks.once("dnd5e.useItem", () => {
@@ -84,4 +94,129 @@ function twinned(itemName = "Metamagic: Twinned Spell") {
             }
         });
     }
+}
+
+/** Registers the chat card buttons and hooks for the Extended Spell Metamagic feature */
+function extended(itemName = "Metamagic: Extended Spell") {
+    const SCALAR_TIME_PERIODS_IN_SECONDS = {
+        day: 60 * 60 * 24,
+        hour: 60 * 60,
+        minute: 60,
+    }
+
+    const MODE_CONFIGS = {
+        warded: {
+            durationMultiplier: 2,
+            minDurationInSeconds: SCALAR_TIME_PERIODS_IN_SECONDS.minute,
+            maxDurationInSeconds: SCALAR_TIME_PERIODS_IN_SECONDS.day
+        },
+        channelled: {
+            durationMultiplier: 10,
+            minDurationInSeconds: SCALAR_TIME_PERIODS_IN_SECONDS.minute,
+            maxDurationInSeconds: SCALAR_TIME_PERIODS_IN_SECONDS.day
+        }
+    }
+
+    ChatCardButtons.register({ itemName, buttons: [
+        { label: "Warded", callback: ({item}) => extendedButton(item, false) },
+        { label: "Channeled", callback: ({item}) => extendedButton(item, true) },
+        rollFatigueButton
+    ]});
+
+    // Chat card button creates the metamagic effect on the actor
+    async function extendedButton(item, channelled=false) {
+        const eff = item.effects
+            .find(e => e.name.includes(channelled ? "(Channelled)" : "(Warded)"));
+        await eff.update({disabled: false});
+    }
+
+    // Pre-Use hook handles the actual work
+    Hooks.on("dnd5e.preUseItem", (item, config, options) => {
+        // Test to break recursion
+        if(foundry.utils.getProperty(options, "flags.talia-custom.isExtended") === true) return;
+
+        // Ensure correct type and parent existance
+        if(!item.actor || item.type !== "spell") return;
+
+        // Ensure the actor has the relevant active effect applied
+        const eff = item?.actor?.appliedEffects?.find(e => e.name.startsWith(itemName));
+        if(!eff) return;
+
+        // Handle cases where an itemData flag has already been set
+        let spellItem = item;
+        const storedData = foundry.utils.getProperty(options, "flags.dnd5e.itemData");
+        if(storedData) {
+            spellItem = new dnd5e.documents.Item5e(storedData, {parent: item.actor})
+        }
+
+        // Verify and validate item
+        const dur = spellItem.system?.duration?.value;
+        if(!Number.isInteger(dur) || dur <= 0) return;
+
+        const timePeriod = spellItem.system?.duration?.units;
+        const timePeriodInSeconds = SCALAR_TIME_PERIODS_IN_SECONDS[timePeriod];
+        if(!timePeriodInSeconds) return;
+
+        // Convert to s as common unit
+        const durInS = timePeriodInSeconds * dur;
+
+        // Get mode from active effect
+        const modeConfig = eff.name.includes("(Channelled)") 
+            ? MODE_CONFIGS.channelled 
+            : MODE_CONFIGS.warded;
+        
+        // Filter effects below minimum duration
+        if(durInS < modeConfig.minDurationInSeconds) return;
+
+        // Calculate new duration & clamp to maximum duration
+        const newDurInS = Math.min(durInS * modeConfig.durationMultiplier, modeConfig.maxDurationInSeconds);
+
+        // Convert back to original unit
+        const newDur = newDurInS / SCALAR_TIME_PERIODS_IN_SECONDS[timePeriod];
+
+        // Cancel the current use, create a cloned item, and use that instead
+        (async () => {
+            // Prepare the modified data to be passed in the options flags
+            const itemData = spellItem.toObject();
+            foundry.utils.mergeObject(itemData, {"system.duration.value": newDur});
+            
+            // Adjust the duration of effects on the item only if their duration matches the duration of the item
+            for(const effData of itemData.effects) {
+                if(effData.duration.seconds === durInS) {
+                    effData.duration.seconds = newDurInS;
+                }
+            }
+            
+            // Create the cloned item to use 
+            // (so the automatically generated concentration effect gets the correct duration too)
+            const itemClone = spellItem.clone({"system.duration.value": newDur}, {keepId: true});
+            itemClone.prepareData();
+            itemClone.prepareFinalAttributes();
+
+            // Set the modified item data with the modified effect data as a flag so
+            // the Item5e._onChatCardAction event handler uses that instead of fetching 
+            // the original item from the actor.
+            foundry.utils.setProperty(options, "flags.dnd5e.itemData", itemData);
+
+            // Set recursion guard
+            foundry.utils.setProperty(options, "flags.talia-custom.isExtended", true);
+            
+            await itemClone.use(config, options);
+
+        })();
+        return false;
+    });
+
+    // Post-Use hook handles cleanup of the metamagic effect on the actor if the metamagic was applied.
+    Hooks.on("dnd5e.useItem", (item, config, options) => {
+        if(foundry.utils.getProperty(options, "flags.talia-custom.isExtended") !== true) return;
+
+        const featureItem = item?.actor?.items.getName(itemName);
+        if(!featureItem) return;
+
+        // Disable all effects (if both were enabled accidentally).
+        for(const eff of featureItem.effects) {
+            if(!eff.disabled) eff.update({disabled: true});
+        }
+    });
 }
