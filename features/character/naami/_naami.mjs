15bc13f9300d781e908e2329b390052d4a1b11e7
Dave <dave.belodri@gmail.com>
2025-08-10 03:14:19 +0200

    Add Metamagic Mastery feature

--- features/character/naami/_naami.mjs
+++ features/character/naami/_naami.mjs
@@ -1,9 +1,11 @@
 import channeledMetamagic from "./channelledMetamagic.mjs";
 import metamagic from "./metamagic.mjs";
+import metamagicMastery from "./metamagicMastery.mjs"
 
 export default {
     registerSubsection() {
         metamagic.register();
         channeledMetamagic.register();
+        metamagicMastery.register();
     }
 }
